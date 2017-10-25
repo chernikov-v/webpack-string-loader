40499b54ff61b77d37dd0b8f4ad2d2ce1f78c385
chernikov-v <4ev.444@gmail.com>
2017-10-25 13:48:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,3 @@ module.exports = function (content) {
 
   return content.indexOf('module.exports =') >=0 ? content : 'module.exports =' + JSON.stringify(content);
 }
-/*
-module.exports.pitch =  function(remainingRequest, precedingRequest, data) {
-      var ss = require(remainingRequest);
-
-      var d = "module.exports = JSON.stringify(require(" + JSON.stringify("-!" + remainingRequest) + "));";
-      debugger;
-      return d
-};
-*/
